ee5e77a316139dd2bdf8cde6d612b36b589208f5
Michael Yudin <mihelitto@gmail.com>
2021-09-20 00:21:56 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from pathlib import Path
+from os import path
+
+IMAGE_FOLDER = 'images'
+
+
+def download_image(image_url, image_name):
+    response = requests.get(image_url)
+    with open(image_name, 'bw') as file:
+        file.write(response.content)
+
+
+def fetch_spacex_last_launch(flight_number):
+    url = f'https://api.spacexdata.com/v3/launches/{flight_number}'
+    response = requests.get(url)
+    launch = response.json()
+    image_urls = launch["links"]["flickr_images"]
+    for num, image_url in enumerate(image_urls):
+        download_image(image_url, path.join(IMAGE_FOLDER, f'spacex{num}.jpg'))
+        print(image_url)
+
+
+def main():
+    Path(IMAGE_FOLDER).mkdir(parents=True, exist_ok=True)
+    flight_number = 107
+    fetch_spacex_last_launch(flight_number)
+
+
+if __name__ == '__main__':
+    main()
