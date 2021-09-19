689304822f9a1a3cbabd0823b06d1f0204293eb8
Michael Yudin <mihelitto@gmail.com>
2021-09-20 01:07:30 +0300

    Add getting nasa apods

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 from pathlib import Path
 from urllib.parse import unquote, urlsplit
 from os import path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 IMAGE_FOLDER = 'images'
+nasa_api_key = env('NASA_API_KEY')
 
 
 def download_image(image_url, image_name):
@@ -29,6 +34,25 @@ def get_file_extension(link):
 
 def main():
     Path(IMAGE_FOLDER).mkdir(parents=True, exist_ok=True)
+    nasa_image_url = 'https://api.nasa.gov/planetary/apod'
+    params = {'count': 30, 'api_key': nasa_api_key}
+    response = requests.get(nasa_image_url, params=params)
+    random_apods = response.json()
+    for num, apod in enumerate(random_apods, 1):
+        try:
+            image_url = apod['hdurl']
+        except KeyError:
+            print('Не удалось получить ссылку на фотографию.')
+            print(apod)
+            continue
+        file_extension = get_file_extension(image_url)
+        download_image(
+            image_url,
+            path.join(IMAGE_FOLDER, f'nasa{num}{file_extension}'),
+        )
+        print(image_url)
+
+    exit()
     flight_number = 107
     fetch_spacex_last_launch(flight_number)
 
