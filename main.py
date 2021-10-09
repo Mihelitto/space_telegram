540c64596f6c6de744942787bdd329e8210eb351
Michael Yudin <mihelitto@gmail.com>
2021-10-09 20:38:18 +0300

    Add getting nasa epic images

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 from pathlib import Path
 from urllib.parse import unquote, urlsplit
@@ -34,6 +36,21 @@ def get_file_extension(link):
 
 def main():
     Path(IMAGE_FOLDER).mkdir(parents=True, exist_ok=True)
+    image_extension = 'png'
+    params = {'api_key': nasa_api_key}
+    epic_images_url = f'https://api.nasa.gov/EPIC/api/natural/images'
+    response = requests.get(epic_images_url, params=params)
+    response.raise_for_status()
+    response_payload = response.json()
+    for photo in response_payload:
+        image_date, _ = photo['date'].split()
+        image_date = datetime.fromisoformat(image_date).date()
+        image_name = photo['image']
+        file_image_name = f'{image_name}.{image_extension}'
+        epic_image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date.year}/{image_date.month:02d}/{image_date.day:02d}/png/{image_name}.png?api_key={"DEMO_KEY"}'
+        download_image(epic_image_url, path.join(IMAGE_FOLDER, file_image_name))
+
+
     nasa_image_url = 'https://api.nasa.gov/planetary/apod'
     params = {'count': 30, 'api_key': nasa_api_key}
     response = requests.get(nasa_image_url, params=params)
@@ -52,7 +69,6 @@ def main():
         )
         print(image_url)
 
-    exit()
     flight_number = 107
     fetch_spacex_last_launch(flight_number)
 
